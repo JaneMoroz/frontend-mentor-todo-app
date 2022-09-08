201fe475dcafbdde9e08956a171de628faddbf0b
JaneMoroz <moroz.jane@yahoo.com>
2022-09-08 19:20:19 +0300

    feat: add new data, similar to design

--- src/assets/data/data.ts
+++ src/assets/data/data.ts
@@ -1,6 +1,17 @@
+import uuid from "react-uuid";
+
 export const todos = [
-  { id: "0", text: "Go shopping", status: "active" },
-  { id: "1", text: "Do laundry", status: "active" },
-  { id: "2", text: "Finish assignments", status: "active" },
-  { id: "3", text: "Fix car noise issue", status: "completed" },
+  {
+    id: uuid(),
+    text: "Complete online Javascript course",
+    status: "completed",
+  },
+  { id: uuid(), text: "Jog around the park 3x", status: "active" },
+  { id: uuid(), text: "10 minutes meditation", status: "active" },
+  { id: uuid(), text: "Pick up groceries", status: "active" },
+  {
+    id: uuid(),
+    text: "Complete Todo App on Frontend Mentor",
+    status: "active",
+  },
 ];
